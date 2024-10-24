cc60eac75da387815b04a3b10c1134a4956c3238
LAPTOP-E8LDI7Q5\chan <dprtmehd@gmail.com>
2024-10-24 15:45:47 +0900

    the app provides temperature and weather information for each region..

---
+++ Weatherinfo/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import NavBar from './components/NavBar.vue';
+  import MainComp from './components/MainComp.vue';
+  import { ref, onMounted } from 'vue';
+  import { useStore } from 'vuex';
+  import About from './components/About.vue';
+
+  const store = useStore();
+  
+
+  
+  // 앱이 실행되면 날씨 데이터 가져오기
+  onMounted(() => {
+    store.dispatch('getWeather');
+    
+  })
+ const onSearchCity = (city) =>{
+
+  weatherData.value.city=city;
+  getWeather();
+ }
+</script>
+
+<template>
+ <button @click="$store.dispatch('getWeather')">getWeather</button>
+ <NavBar />
+ 
+  <div v-if="!$store.state.toggle">
+    <MainComp />
+  </div>
+  <div v-else>
+    <About />
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
